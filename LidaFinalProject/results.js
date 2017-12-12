c17427459dac580b3dfe959910ef2b0a4ed46588
masonfarmer <33735563+masonfarmer@users.noreply.github.com>
2017-12-12 17:39:28 -0500

    .

---
+++ LidaFinalProject/results.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  console.log("The js is hooked up");
+  // alert(localStorage.sessionId)
+});
+
+
+
+API_URL = "http://ec2-18-217-72-185.us-east-2.compute.amazonaws.com:8080/PossiblePrograms/?sessionId=" + sessionId;
+alert(API_URL);
+var xhr = createCORSRequest('GET', API_URL);
+xhr.responseType = 'text';
+if (!xhr) {
+ alert('CORS not supported');
+ return;
+}
+// Response handlers.
+xhr.onload = function() {
+  var jsonResponse = JSON.parse(xhr.responseText);
+  courses = jsonResponse.Courses;
+  alert(courses);
+};
+
+xhr.onerror = function() {
+    alert('FAILURE');
+};
+
+xhr.send();
